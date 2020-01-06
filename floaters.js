27be41215076176bccf0481dcda602f36ffe217e
Plasnid <stuart@sackler.ca>
2020-01-06 01:08:25 -0500

    initial file upload

---
+++ floaters.js
@@ -0,0 +1,62 @@
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+ctx.canvas.width = window.innerWidth;
+ctx.canvas.height = window.innerHeight;
+
+let particleArray;
+
+function Particle(x, y, directionX, directionY, size, color){
+    this.x = x;
+    this.y = y;
+    this.directionX = directionX;
+    this.directionY = directionY;
+    this.size = size;
+    this.color = color;
+}
+
+Particle.prototype.draw = function(){
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI*2, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+}
+
+Particle.prototype.update = function(){
+    if(this.x + this.size > canvas.width || this.x - this.size < 0){
+        this.directionX = -this.directionX;
+    }
+    if(this.y + this.size > canvas.height || this.y - this.size < 0){
+        this.directionY = -this.directionY;
+    }
+    this.x +=this.directionX;
+    this.y +=this.directionY;
+    this.draw();
+}
+
+function animate(){
+    requestAnimationFrame(animate);
+    ctx.clearRect(0,0,innerWidth,innerHeight);
+    for(let i=0;i<particleArray.length;i++){
+        particleArray[i].update();
+    }
+}
+
+function init(){
+    particleArray = [];
+    for(let i=0;i<100;i++){
+        let size = Math.random()*20;
+        let x = Math.random() * (innerWidth - size *2);
+        let y = Math.random() * (innerHeight - size *2);
+        let directionX = (Math.random() * 0.4) - 0.2;
+        let directionY = (Math.random() * 0.4) - 0.2;
+        let color = `rgb(${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)})`;
+        //let color = 'white';
+        particleArray.push(new Particle(x,y,directionX,directionY,size,color));
+    }
+}
+
+
+init();
+animate();
+//const particle1 = new Particle(100,100,1,1,20,"white");
+//particle1.draw();
